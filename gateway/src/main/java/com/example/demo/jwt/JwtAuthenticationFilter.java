973aefd8b39c9757db14b6ee35849911b0a77aca
sougata <sougata19582001@gmail.com>
2025-11-17 23:48:45 +0530

    added some basic headers and token check in jwt auth filter.

--- gateway/src/main/java/com/example/demo/jwt/JwtAuthenticationFilter.java
+++ gateway/src/main/java/com/example/demo/jwt/JwtAuthenticationFilter.java
@@ -1,6 +1,8 @@
 package com.example.demo.jwt;
 
+import com.example.demo.errors.exceptions.UnAuthorizedAccessException;
 import lombok.RequiredArgsConstructor;
+import org.apache.http.HttpHeaders;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Configuration;
@@ -15,9 +17,29 @@ public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
-        System.out.println("JWT FILTER");
-        System.out.println(request.getURI().getPath());
-        System.out.println("JWT FILTER");
+
+        // skip authentication for these paths
+        if (request.getURI().getPath().contains("/auth")){
+            return chain.filter(exchange);
+        }
+
+        // check for Auth headers
+        if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)){
+            throw new UnAuthorizedAccessException("Authorization Headers are missing");
+        }
+
+        String authHeader = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+        if (authHeader == null || !authHeader.startsWith("Bearer ")){
+            throw new UnAuthorizedAccessException("Bearer Token is missing");
+        }
+
+        String token = authHeader.substring(7);
+        try {
+            // validate the token here.
+            System.out.println("validating token");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         return chain.filter(exchange);
     }
 
