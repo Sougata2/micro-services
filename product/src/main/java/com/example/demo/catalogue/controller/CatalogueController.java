669958123651565bee9d9f80557dfa83f95d776e
sougata <sougatadas@cdac.in>
2025-11-17 17:37:34 +0530

    initial commit

---
+++ product/src/main/java/com/example/demo/catalogue/controller/CatalogueController.java
@@ -0,0 +1,18 @@
+package com.example.demo.catalogue.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalogues")
+public class CatalogueController {
+
+    @GetMapping("/all")
+    public ResponseEntity<List<String>> getCatalogues(){
+        return ResponseEntity.ok(List.of("Catalogue1", "Catalogue2", "Catalogue3"));
+    }
+}
