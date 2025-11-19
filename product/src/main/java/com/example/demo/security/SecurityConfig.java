25940e76fcf0187bf359812b0a552dc1afe2681f
sougata <sougatadas@cdac.in>
2025-11-19 17:22:21 +0530

    added security config and appended auth filter from auth common in add filter before.

---
+++ product/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.demo.security;
+
+import com.domain.authCommon.filter.AuthFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final AuthFilter authFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                ).addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
